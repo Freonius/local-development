8a56f9306eb7739282dd9af36b6d619d1a09b13f
Fred Pirani <freonius@gmail.com>
2023-06-01 22:42:32 +0200

    Local secrets

--- src/__main__.py
+++ src/__main__.py
@@ -1,9 +1,37 @@
+from os import environ
+from contextlib import suppress
+from json import dumps
 from fastapi import FastAPI
 from uvicorn import run
 from .schema import BasicResponse
 from .utils import SecretsManager
 
+if __debug__:
+    # Let's set the local environment
+    environ["AWS_ENDPOINT"] = "http://localhost:4566"
+    environ["AWS_ACCESS_KEY"] = "localhost"
+    environ["AWS_SECRET_ACCESS_KEY"] = "localhost"
+    environ["AWS_DEFAULT_REGION"] = "eu-west-1"
+
 sec_man: SecretsManager = SecretsManager()
+
+if __debug__:
+    with suppress(Exception):
+        # It will throw an exception if the secret is already set
+        # ...so we will suppress it
+        sec_man.client.create_secret(
+            Name="mySecret",
+            SecretString=dumps(
+                {
+                    "host": "localhost",
+                    "port": "5432",
+                    "user": "localdev",
+                    "password": "robinhood",
+                    "name": "localdev",
+                }
+            ),
+        )
+
 sec_man.get_secrets("mySecret")
 
 app: FastAPI = FastAPI()
